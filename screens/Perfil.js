bd20031cb1a353bfabb969d6974d9d188becaa6a
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-03-03 22:50:44 -0300

    Atualização - 22500303

---
+++ screens/Perfil.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Perfil({navigation}) {
+
+    const logout = (navigation) => {
+        AsyncStorage.setItem("TOKEN","").then(() => {
+            navigation.reset({
+                index: 0,
+                routes: [{name: "Login"}]
+            })
+        }).catch((error) => {
+            console.log(error)
+            Alert.alert("Erro ao sair")
+        })
+    }
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Profile!</Text>
+        <Button
+            title="Sair"
+            onPress={() => logout(navigation)}
+            icon={{
+              name: "check",
+              type: "font-awesome",
+              size: 15,
+              color: "white",
+            }}
+            iconContainerStyle={{ marginRight: 10 }}
+            titleStyle={{ fontWeight: "700" }}
+            buttonStyle={{
+              backgroundColor: "rgba(90, 154, 230, 1)",
+              borderColor: "transparent",
+              borderWidth: 0,
+              borderRadius: 30,
+            }}
+            containerStyle={{
+              width: 200,
+              marginHorizontal: 85,
+              marginVertical: 10,
+            }}
+          />
+      </View>
+    );
+  }
--- screens/Principal.js
+++ screens/Principal.js
@@ -1,112 +1,73 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Button } from 'react-native-elements';
 
-const logout = (navigation) => {
-  navigation.reset({
-    index: 0,
-    routes: [{name: "Login"}]
-  })
-}
+import Perfil from './Perfil';
+import Busca from './Busca';
+import Produtos from './Produtos';
+import Servicos from './Servicos';
+import Cadastrar from './Cadastrar';
 
-function Feed() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
-      
-    </View>
-  );
-}
-
-function Profile({navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
-      <Button
-          title="Sair"
-          onPress={() => logout(navigation)}
-          icon={{
-            name: "check",
-            type: "font-awesome",
-            size: 15,
-            color: "white",
-          }}
-          iconContainerStyle={{ marginRight: 10 }}
-          titleStyle={{ fontWeight: "700" }}
-          buttonStyle={{
-            backgroundColor: "rgba(90, 154, 230, 1)",
-            borderColor: "transparent",
-            borderWidth: 0,
-            borderRadius: 30,
-          }}
-          containerStyle={{
-            width: 200,
-            marginHorizontal: 85,
-            marginVertical: 10,
-          }}
-        />
-    </View>
-  );
-}
-
-function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-    </View>
-  );
-}
-
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Principal() {
- 
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
-      activeColor="#e91e63"
-      labelStyle={{ fontSize: 12 }}
-      style={{ backgroundColor: 'tomato' }}
+      initialRouteName="Busca"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
     >
       <Tab.Screen
-        name="Feed"
-        component={Feed}
+        name="Busca"
+        component={Busca}
         options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
+          tabBarLabel: 'Buscar',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-search" color={color} size={size} />
           ),
         }}
       />
       <Tab.Screen
-        name="Notifications"
-        component={Notifications}
+        name="Produtos"
+        component={Produtos}
         options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          tabBarLabel: 'Produtos',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="shopping" color={color} size={size} />
           ),
         }}
       />
       <Tab.Screen
-        name="Profile"
-        component={Profile}
+        name="Serviços"
+        component={Servicos}
         options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
+          tabBarLabel: 'Serviços',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="human-greeting" color={color} size={size} />
           ),
         }}
       />
-     
-     
+      <Tab.Screen
+        name="Cadastrar"
+        component={Cadastrar}
+        options={{
+          tabBarLabel: 'Cadastrar',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus-circle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-
-    
-    
   );
 }
